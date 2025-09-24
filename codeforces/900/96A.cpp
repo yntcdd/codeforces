f62126e3ec7f3c9700ae780ad078c1878488763b
yntcdd <you@example.com>
2025-09-23 21:23:29 -0400

    First 900

---
+++ codeforces/900/96A.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string players;
+    int counter = 1;
+    string dangerous = "NO";
+
+    cin >> players;
+
+    
+    for (int i = 0; i < players.size(); i++) {
+        if (players[i] == players[i + 1]) {
+            counter++;
+            if (counter >= 7) {
+                dangerous = "YES";
+                break;
+            }
+        } else {
+            counter = 1;
+        }
+    }
+
+    cout << dangerous;
+
+    return 0;
+}
