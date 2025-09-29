937e117c87960beb0d0189e5ec9b68b0f8cffe9d
yntcdd <michaelzhang935@gmail.com>
2025-09-29 19:44:59 -0400

    pangram

---
+++ codeforces/800/520A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <cctype>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string word;
+    cin >> word;
+
+    if (n < 26) {
+        cout << "NO\n";
+        return 0;
+    }
+
+    set<char> letters;
+    for (char ch : word) {
+        letters.insert(toupper(ch));
+    }
+
+    if (letters.size() == 26) cout << "YES\n";
+    else cout << "NO\n";
+
+    return 0;
+}
