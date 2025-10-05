e27b1bb23ab4a093f23c40e91dcb11b9af88a0e8
yntcdd <michaelzhang935@gmail.com>
2025-10-05 13:30:31 -0400

    FIrst contest

---
+++ codeforces/contest/2155B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+        
+    }
+
+    return 0;
+}
