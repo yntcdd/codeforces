160682fbd406b9706bdf8156e7a4a1cbb47e23e4
yntcdd <michaelzhang935@gmail.com>
2025-10-05 08:54:45 -0400

    Gravity Flip

---
+++ codeforces/900/405A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    int nums[n] = {};
+
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    sort(nums, nums+n);
+
+    for (int  i = 0; i < n; i ++) {
+        cout << nums[i] << " ";
+    }
+
+    return 0;
+}
