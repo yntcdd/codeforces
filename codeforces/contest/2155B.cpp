73c6a4d359c9091d46b22acd8f4208885f35edc5
yntcdd <michaelzhang935@gmail.com>
2025-10-05 13:54:32 -0400

    c

--- codeforces/contest/2155B.cpp
+++ codeforces/contest/2155B.cpp
@@ -9,7 +9,36 @@ int main() {
     cin >> t;
 
     for (int i = 0; i < t; i++) {
-        
+        int n, k;
+
+        cin >> n >> k;
+
+        if (n * n - k == 1) {
+            cout << "NO" << endl;
+        } else {
+            cout << "YES" << endl;
+            int counter = 0;
+            for (int row = 0; row < n; row++) {
+                string layer;
+                for (int column = 0; column < n; column++) {
+                    counter++;
+                    if (counter <= k) {
+                        layer += 'U';
+                    } else {
+                        if (column == n - 1) {
+                            if (row != n- 1) {
+                                layer += 'D';
+                            } else{
+                                layer += 'L';
+                            }
+                        } else {
+                            layer += 'R';
+                        }
+                    }
+                }
+                cout << layer << endl;
+            }
+        }
     }
 
     return 0;
