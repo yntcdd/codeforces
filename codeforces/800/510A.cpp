c2c723195c743e53b6179a395e818c1d0aeeb271
yntcdd <michaelzhang935@gmail.com>
2025-10-09 18:59:59 -0400

    first

--- codeforces/800/510A.cpp
+++ codeforces/800/510A.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -9,7 +10,27 @@ int main() {
     cin >> n >> m;
 
     for (int i = 0; i < n; i++) {
-
+        string row;
+        bool which = true;
+        if (i % 2 == 0) {
+            for (int i = 0; i < m; i++) {
+                row += '#';
+            }
+        } else {
+            if (which) {
+                for (int i = 0; i < m - 1; i++) {
+                    row += '.';
+                }
+                row += '#';
+                which = false;
+            } else {
+                row += '#';
+                for (int i = 0; i < m - 1; i++) {
+                    row += '.';
+                }
+            }
+        }
+        cout << row << endl;
     }
 
     return 0;
