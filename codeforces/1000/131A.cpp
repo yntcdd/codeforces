0103ea825f16ae74f64a70e77742ad60bdaeaa01
yntcdd <michaelzhang935@gmail.com>
2025-10-14 20:32:52 -0400

    cAPS lOCK

--- codeforces/1000/131A.cpp
+++ codeforces/1000/131A.cpp
@@ -9,16 +9,34 @@ int main() {
 
     cin >> word;
 
-    for (int i = 0; i < word.size(); i++) {
-        if (word[0] > 90) {
-            word[0] -= 32;
-        }
-        if (word[i] < 91 && i != 0) {
-            word[i] += 32;
-        }
+    string special = word;
+    string uncap = word;
+
+    bool first = false;
+    int letters = 0;
+
+    if (word[0] < 91) {
+        first = true;
+        letters++;
+        uncap[0] += 32;
+    } else {
+        special[0] -= 32;
     }
 
-    cout << word;
+    for (int i = 1; i < word.size(); i++) {
+        if (word[i] < 91) {
+            letters++;
+            special[i] += 32;
+            uncap[i] += 32;
+        }
+    }
+    if (word.size() == letters) { 
+        cout << uncap;
+    } else if (first == false && letters == word.size() - 1) {
+        cout << special;
+    } else {
+        cout << word;
+    }
 
     return 0;
 }
