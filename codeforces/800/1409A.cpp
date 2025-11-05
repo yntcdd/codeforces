36f3a85eec93b54c07432753b0e9029b9e1780da
yntcdd <michaelzhang935@gmail.com>
2025-11-05 07:32:52 -0500

    Remove Smallest

--- codeforces/800/1409A.cpp
+++ codeforces/800/1409A.cpp
@@ -9,8 +9,6 @@ int main() {
         long long a, b;
         cin >> a >> b;
         long long diff = llabs(a - b);
-        // Each move you can change a by any k from 1 to 10 (add or subtract),
-        // so in effect you can move up to 10 each move toward b.
         long long moves = diff / 10;
         if (diff % 10 != 0) {
             moves += 1;
