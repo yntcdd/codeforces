b7d996304abab8897177fea834a5bfff5695d78f
yntcdd <michaelzhang935@gmail.com>
2025-11-09 21:27:41 -0500

    Odd One Out

---
+++ codeforces/800/1915A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a == b) {
+            cout << c << "\n";
+        } else if (a == c) {
+            cout << b << "\n";
+        } else {
+            cout << a << "\n";
+        }
+    }
+    return 0;
+}
