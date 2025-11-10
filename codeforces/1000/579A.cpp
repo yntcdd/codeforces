941cc56bc7ec2d73fdd28bc4e0b56bf881b56942
yntcdd <michaelzhang935@gmail.com>
2025-11-09 21:25:06 -0500

    Raising Bacteria

---
+++ codeforces/1000/579A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int count = 0;
+    while (n > 0) {
+        if (n % 2 == 1)
+            count++;
+        n /= 2;
+    }
+
+    cout << count << endl;
+    return 0;
+}
