67d218a8fe18025fd4b8db6b29dfe4e2d81bfe89
yntcdd <michaelzhang935@gmail.com>
2025-11-22 09:46:55 -0500

    Maximum GCD

---
+++ codeforces/800/1370A.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    // Maximum GCD you can get from two numbers <= n
+    cout << (n / 2) << "\n";
+    return 0;
+}
