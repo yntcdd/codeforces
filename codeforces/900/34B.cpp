5b75b5756d53724d85af0029bf1471dbfba9ab0d
yntcdd <michaelzhang935@gmail.com>
2025-11-22 09:43:13 -0500

    Sale

--- codeforces/900/34B.cpp
+++ codeforces/900/34B.cpp
@@ -12,20 +12,17 @@ int main() {
         cin >> prices[i];
     }
 
-    // Sort TV prices in ascending order
     sort(prices.begin(), prices.end());
 
     int earned = 0;
-    // Take at most m TV sets with negative price
     for (int i = 0; i < m; i++) {
         if (prices[i] < 0) {
             earned += prices[i];
         } else {
-            break; // as soon as we reach a non-negative price, stop
+            break; 
         }
     }
 
-    // Since earned is negative or zero, make it positive by multiplying by -1
     cout << -earned << "\n";
 
     return 0;
