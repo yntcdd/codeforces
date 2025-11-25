e7fc331391713de4975ebbefc2baaa84146f7df6
yntcdd <michaelzhang935@gmail.com>
2025-11-24 19:04:59 -0500

    Two Buttons

---
+++ codeforces/1400/520B.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    int steps = 0;
+    while (m > n) {
+        if (m % 2 == 0) {
+            m /= 2;
+        } else {
+            m += 1;
+        }
+        steps++;
+    }
+    steps += (n - m);
+
+    cout << steps << "\n";
+    return 0;
+}
