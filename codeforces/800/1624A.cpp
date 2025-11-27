21c8778a133c24d266ac1d931c38144e36a59e3c
yntcdd <michaelzhang935@gmail.com>
2025-11-27 17:19:18 -0500

    Plus One on the Subset

--- codeforces/800/1624A.cpp
+++ codeforces/800/1624A.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,7 +9,16 @@ int main() {
     cin >> t;
 
     while (t--) {
+        int n;
+        cin >> n;
 
+        int a[n] = {};
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        sort(a, a+n);
+
+        cout << a[n-1] - a[0] << endl;
     }
 
     return 0;
