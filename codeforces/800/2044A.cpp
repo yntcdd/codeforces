7bd894792e3ead857efd50d6419faf9b38233c1a
yntcdd <michaelzhang935@gmail.com>
2025-12-03 22:20:28 -0500

    Easy Problem

---
+++ codeforces/800/2044A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int x;
+        cin >> x;
+ 
+        cout << x - 1 << endl;
+    }
+ 
+    return 0;
+}
