357c4cfc54d44fd4bd0cf896a3bc98cc8c661b2f
yntcdd <michaelzhang935@gmail.com>
2025-12-04 18:28:58 -0500

    New Year's Number

---
+++ codeforces/900/1475B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int t; 
+    cin >> t;
+
+    while(t--) {
+        int n; cin >> n;
+        if (n / 2020 >= n % 2020) cout << "YES\n";
+        else cout << "NO\n";
+    }
+
+    return 0;
+}
