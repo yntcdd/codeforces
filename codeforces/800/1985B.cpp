5869cdd174370ec9aa4909bb7bebd34c0d6c12f5
yntcdd <michaelzhang935@gmail.com>
2025-12-05 19:44:55 -0500

    Maximum Multiple Sum

---
+++ codeforces/800/1985B.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+ 
+
+using namespace std;
+
+int main() {
+    
+    int t;
+
+    cin >> t;
+
+    while (t--) {
+        int x;
+        cin >> x;
+
+        if (x == 3) {
+            cout << 3 << endl;
+        } else {
+            cout << 2 << endl;
+        }
+        
+    }
+
+    return 0;
+}
