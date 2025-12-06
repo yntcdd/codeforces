817df1b6db0bda345babb61dd31157fcb6e54c1b
yntcdd <you@example.com>
2025-12-06 11:28:30 -0500

    Colourblindness

---
+++ codeforces/800/1722B.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        string first;
+        string second;
+
+        cin >> first >> second;
+        bool yes = true;
+        for (int i = 0; i < n; i++) {
+            if (first[i] != second[i]) {
+                if (first[i] == 'R' || second[i] == 'R') {
+                    yes = false;
+                }
+            }
+        }
+        if (yes) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
