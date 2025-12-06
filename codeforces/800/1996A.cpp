564bba62fde33ccc73eb0e34521f907c9ee932be
yntcdd <you@example.com>
2025-12-06 10:52:51 -0500

    Legs

---
+++ codeforces/800/1996A.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int t;
+
+    cin >> t;
+    
+    while (t--) {
+        int n;
+        cin >> n;
+
+        if (n == 2) {
+            cout << 1 << endl;
+        } else {
+            if (n % 4 == 0) {
+                cout << n / 4 << endl;
+            } else {
+                cout << n / 4 + 1 << endl;
+            }
+        }
+    }
+
+    return 0;
+}
