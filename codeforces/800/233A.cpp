c6d8fc8c8a369d02a82fae74f1115e872aebf74b
yntcdd <michaelzhang935@gmail.com>
2025-12-22 10:23:54 -0500

    Perfect Permutation

---
+++ codeforces/800/233A.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    if (n % 2 == 1) {
+        cout << -1;
+        return 0;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+            cout << i - 1 << " ";
+        } else {
+            cout << i + 1 << " ";
+        }
+    }
+
+    return 0;
+}
