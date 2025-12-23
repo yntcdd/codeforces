064e1380f432780ac5030a3a0bddf6cc89586f60
yntcdd <michaelzhang935@gmail.com>
2025-12-23 11:27:02 -0500

    C

--- codeforces/contest/2179C.cpp
+++ codeforces/contest/2179C.cpp
@@ -1,14 +1,32 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
 
-    int t;
+    int t;  
     cin >> t;
-
     while (t--) {
+        int n;
+        cin >> n;
+        int a[n] = {};
 
+        int smallest = 10000000;
+        int secondsmallest = 10000000;
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+            smallest = min(smallest, a[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            if (a[i] != smallest) {
+                secondsmallest = min(secondsmallest, a[i]);
+            }
+        }
+
+        if (secondsmallest - smallest > smallest) {
+            cout << secondsmallest - smallest << endl;
+        } else {
+            cout << smallest << endl;
+        }
     }
 
     return 0;
