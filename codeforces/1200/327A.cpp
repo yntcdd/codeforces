0166e3a51269f1ed818cdecfd1718344038f804e
yntcdd <michaelzhang935@gmail.com>
2025-12-23 09:49:17 -0500

    A

--- codeforces/1200/327A.cpp
+++ codeforces/1200/327A.cpp
@@ -5,24 +5,29 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-
+    
     int a[n];
+    int totalOnes = 0;
+    
     for (int i = 0; i < n; i++) {
         cin >> a[i];
-    }
-
-    int totalOnes = 0;
-    for (int i = 0; i < n; i++) {
         if (a[i] == 1) totalOnes++;
     }
-
-    int cur = 0, best = 0;
+    
+    // If all are 1s, flipping any non-empty segment will reduce count by at least 1
+    if (totalOnes == n) {
+        cout << totalOnes - 1 << endl;
+        return 0;
+    }
+    
+    int cur = 0, bestGain = -1e9;
+    
     for (int i = 0; i < n; i++) {
         int val = (a[i] == 0 ? 1 : -1);
         cur = max(val, cur + val);
-        best = max(best, cur);
+        bestGain = max(bestGain, cur);
     }
-
-    cout << totalOnes + best;
+    
+    cout << totalOnes + bestGain << endl;
     return 0;
 }
