266bd1f7c27ad5f33f05cd196c91cf25186555cb
yntcdd <michaelzhang935@gmail.com>
2025-12-26 15:01:13 -0500

    Only One Digit

---
+++ codeforces/800/2126A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+
+        int smallest = s[0] - '0';
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] - '0' < smallest) {
+                smallest = s[i] - '0';
+            }
+        }
+
+        cout << smallest << endl;
+    }
+
+    return 0;
+}
