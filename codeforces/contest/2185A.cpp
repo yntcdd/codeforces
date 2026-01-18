1c3b05a14d9174567f3c7d508c4d3eb1cfb94611
yntcdd <michaelzhang935@gmail.com>
2026-01-18 09:42:28 -0500

    A

--- codeforces/contest/2185A.cpp
+++ codeforces/contest/2185A.cpp
@@ -8,7 +8,13 @@ int main() {
     cin >> t;
 
     while (t--) {
+        int n;
+        cin >> n;
 
+        for (int i = 0; i < n; i++) {
+            cout << i + 1 << " ";
+        }
+        cout << "\n";
     }    
 
     return 0;
