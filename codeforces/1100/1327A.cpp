a2fdf8b0a316bb2cb05143e869cfff4b26ba3b37
yntcdd <michaelzhang935@gmail.com>
2026-01-24 13:16:31 -0500

    Sum of Odd Integers

---
+++ codeforces/1100/1327A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n, k;Sum of Odd Integers
+        cin >> n >> k;
+
+        if (k * k <= n && (n % 2 == k % 2))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
