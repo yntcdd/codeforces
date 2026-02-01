064237b14fc83931382b5506e052c1c5161a6deb
yntcdd <michaelzhang935@gmail.com>
2026-01-31 21:20:37 -0500

    Ski Resort

---
+++ codeforces/1000/1840C.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n, k;
+        long long q;
+        cin >> n >> k >> q;
+
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        long long ans = 0;
+        long long len = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] <= q) {
+                // extend current block
+                len++;
+            } else {
+                // when block ends, count segments if long enough
+                if (len >= k) {
+                    long long x = len - k + 1;
+                    ans += x * (x + 1) / 2;
+                }
+                len = 0;
+            }
+        }
+        // last block
+        if (len >= k) {
+            long long x = len - k + 1;
+            ans += x * (x + 1) / 2;
+        }
+
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
