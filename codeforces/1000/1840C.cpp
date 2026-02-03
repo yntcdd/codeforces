e66ffba59f565703d65bafe6101d70a47de733bc
yntcdd <michaelzhang935@gmail.com>
2026-02-02 19:10:21 -0500

    1

--- codeforces/1000/1840C.cpp
+++ codeforces/1000/1840C.cpp
@@ -15,7 +15,7 @@ int main(){
         vector<long long> a(n);
         for (int i = 0; i < n; i++) {
             cin >> a[i];
-        }
+        } 
 
         long long ans = 0;
         long long len = 0;
