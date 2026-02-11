8d3fc23ce26c1312e93ba4c3dcea0fc474c8dae2
yntcdd <michaelzhang935@gmail.com>
2026-02-10 20:30:44 -0500

    Different String

---
+++ codeforces/1100/1476A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n, k;
+        cin >> n >> k;
+
+        
+    }    
+
+    return 0;
+}
