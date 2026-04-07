a25a3d2bca2623581f6eb0b13059f0b554c3b89c
yntcdd <michaelzhang935@gmail.com>
2026-04-07 19:07:47 -0400

    Measurement

---
+++ usaco/measurement.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int N;
+    cin >> N;
+
+    vector<int> day(N);
+    vector<int> name(N);
+    vector<int> change(N);
+
+    
+
+    return 0;
+}
